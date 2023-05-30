d90dd638aa4c50f9ddd418c6a538603a26704666
erik.ibarra.leos@gmail.com <fernandodilland@pm.me>
2023-05-30 01:49:13 -0600

    uPDATE

--- content.js
+++ content.js
@@ -183,7 +183,7 @@ async function extractTextFromWordFile(file) {
 // Submit conversation function
 const submitFilePartText = chrome.i18n.getMessage("submitFilePartText");
 async function submitConversation(text, part, filename) {
-  const textarea = document.querySelector("textarea[tabindex='0']");
+  const textarea = document.querySelector("textarea[tabindex='0'], textarea[aria-label='Input for prompt text']");
   const enterKeyEvent = new KeyboardEvent("keydown", {
     bubbles: true,
     cancelable: true,
@@ -199,7 +199,7 @@ async function submitConversation(text, part, filename) {
 
 // Periodically check if the button has been added to the page and add it if it hasn't
 const targetSelector =
-  ".flex.flex-col.w-full.py-2.flex-grow.md\\:py-3.md\\:pl-4";
+  ".flex.flex-col.w-full.py-2.flex-grow.md\\:py-3.md\\:pl-4, mat-form-field";
 const intervalId = setInterval(() => {
   const targetElement = document.querySelector(targetSelector);
   if (targetElement && !targetElement.contains(buttonWrapper)) {
