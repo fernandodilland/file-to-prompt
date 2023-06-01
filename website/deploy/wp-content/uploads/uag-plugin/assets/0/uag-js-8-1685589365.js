89edbcb5d10943b49530806c1eec9d3b5c68a893
erik.ibarra.leos@gmail.com <fernandodilland@pm.me>
2023-05-31 21:22:09 -0600

    Update website

---
+++ website/deploy/wp-content/uploads/uag-plugin/assets/0/uag-js-8-1685589365.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'DOMContentLoaded', function() {
+	UAGBInlineNotice.init( {"c_id":null,"cookies":false,"close_cookie_days":1,"noticeDismiss":"","icon":"rectangle-xmark"}, '.uagb-block-3943eb85' );
+});
+window.addEventListener( 'DOMContentLoaded', function() {
+	UAGBInlineNotice.init( {"c_id":null,"cookies":false,"close_cookie_days":1,"noticeDismiss":"","icon":"rectangle-xmark"}, '.uagb-block-85721bca' );
+});
+window.addEventListener( 'DOMContentLoaded', function() {
+	UAGBInlineNotice.init( {"c_id":null,"cookies":false,"close_cookie_days":1,"noticeDismiss":"","icon":"rectangle-xmark"}, '.uagb-block-a4043576' );
+});
+ });
